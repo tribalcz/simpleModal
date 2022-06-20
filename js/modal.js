f13af7c3dca0e9a97c039c75ba48de442591f94c
Martin Suchodol <tribal.cz@gmail.com>
2022-06-20 20:22:20 +0200

    Added function for automatic opening of modal window after page loading

--- js/modal.js
+++ js/modal.js
@@ -3,6 +3,11 @@ const toggleModal = () => {
         .classList.toggle('modal_hidden');
 }
 
+
+//By uncommenting, the modal window will open automatically when the page is loaded.
+//Zároveň je nutné zakomentovat nebo odstranit tlačítko a js controll tlačítka modálního okna
+//window.onload = toggleModal();
+
 document.querySelector('#show-modal')
     .addEventListener('click', toggleModal);
 
